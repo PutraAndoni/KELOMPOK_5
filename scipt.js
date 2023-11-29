0015e3df31fff76f760b1eb68c4d87fdb864259e
raishaauwliya <raisha.auwliya123@gmail.com>
2023-11-29 18:19:41 +0700

    revisian

--- scipt.js
+++ scipt.js
@@ -1,46 +1,89 @@
-document.getElementById('fileInput').addEventListener('change', handleFileSelect);
+document.getElementById('generateGrid').addEventListener('click', generateGrid);
 
-function handleFileSelect(event) {
-    const files = event.target.files;
+function generateGrid() {
+    const files = document.getElementById('fileInput').files;
+    const gridColumns = parseInt(document.getElementById('gridColumns').value);
 
-    for (const file of files) {
-        const reader = new FileReader();
-
-        reader.onload = function (e) {
-            const img = new Image();
-            img.src = e.target.result;
-
-            img.onload = function () {
-                const gridSize = 3; // Ubah sesuai dengan jumlah grid yang diinginkan
-                const canvas = document.createElement('canvas');
-                const ctx = canvas.getContext('2d');
-
-                const width = img.width / gridSize;
-                const height = img.height / gridSize;
-
-                canvas.width = img.width;
-                canvas.height = img.height;
-
-                for (let row = 0; row < gridSize; row++) {
-                    for (let col = 0; col < gridSize; col++) {
-                        ctx.drawImage(
-                            img,
-                            col * width,
-                            row * height,
-                            width,
-                            height,
-                            col * width,
-                            row * height,
-                            width,
-                            height
-                        );
-                    }
-                }
-
-                document.getElementById('photoGrid').appendChild(canvas);
-            };
-        };
-
-        reader.readAsDataURL(file);
+    if (files.length === 0) {
+        alert('Please select an image.');
+        return;
     }
+
+    const file = files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const img = new Image();
+        img.src = e.target.result;
+
+        img.onload = function () {
+            const container = document.getElementById('photoGrid');
+            container.innerHTML = ''; // Clear previous content
+
+            const width = img.width / gridColumns;
+            const height = img.height / Math.ceil(img.height / width);
+
+            for (let row = 0; row < Math.ceil(img.height / height); row++) {
+                for (let col = 0; col < gridColumns; col++) {
+                    const canvas = document.createElement('canvas');
+                    const ctx = canvas.getContext('2d');
+
+                    canvas.width = width;
+                    canvas.height = height;
+
+                    ctx.drawImage(
+                        img,
+                        col * width,
+                        row * height,
+                        width,
+                        height,
+                        0,
+                        0,
+                        width,
+                        height
+                    );
+
+                    container.appendChild(canvas);
+                }
+            }
+
+            // Show download button
+            document.getElementById('downloadLink').style.display = 'block';
+        };
+    };
+
+    reader.readAsDataURL(file);
+}
+
+document.getElementById('downloadButton').addEventListener('click', downloadGrid);
+
+function downloadGrid() {
+    const container = document.getElementById('photoGrid');
+    const canvasList = container.getElementsByTagName('canvas');
+
+    const finalCanvas = document.createElement('canvas');
+    const finalCtx = finalCanvas.getContext('2d');
+
+    finalCanvas.width = container.clientWidth;
+    finalCanvas.height = container.clientHeight;
+
+    let offsetX = 0;
+    let offsetY = 0;
+
+    for (const canvas of canvasList) {
+        finalCtx.drawImage(canvas, offsetX, offsetY);
+        offsetX += canvas.width + 10; // 10px gap
+        if (offsetX + canvas.width > finalCanvas.width) {
+            offsetX = 0;
+            offsetY += canvas.height + 10; // 10px gap
+        }
+    }
+
+    // Convert final canvas to data URL
+    const dataURL = finalCanvas.toDataURL();
+
+    // Create a link and trigger the download
+    const downloadLink = document.getElementById('downloadLink');
+    downloadLink.href = dataURL;
+    downloadLink.click();
 }
